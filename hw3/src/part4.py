f169f2fa92a2d3e6901bd4218ab4dc92b9fb463b
Yuan <r10921061@ntu.edu.tw>
2022-04-21 19:41:17 +0800

    part4 ver1

--- hw3/src/part4.py
+++ hw3/src/part4.py
@@ -21,18 +21,48 @@ def panorama(imgs):
     last_best_H = np.eye(3)
     out = None
 
+    current_w = 0
+    orb = cv2.ORB_create()
+    bf = cv2.BFMatcher_create(cv2.NORM_HAMMING)
     # for all images to be stitched:
     for idx in range(len(imgs) - 1):
         im1 = imgs[idx]
         im2 = imgs[idx + 1]
 
         # TODO: 1.feature detection & matching
+        keypoints1, descriptors1 = orb.detectAndCompute(im1, None)
+        keypoints2, descriptors2 = orb.detectAndCompute(im2, None)
+        matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+        good = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append((keypoints1[m.queryIdx].pt, keypoints2[m.trainIdx].pt))
+        good = np.array(good)
 
         # TODO: 2. apply RANSAC to choose best H
+        num_points = len(good)
+        num_best_inliers = 0
+        best_H = np.identity(3)
+        for i in range(2000):
+            points = np.array(good[random.sample(range(len(good)), 4)])
+            H = solve_homography(points[:, 1], points[:, 0])
+            if np.linalg.matrix_rank(H) >= 3:
+                all_u = np.concatenate((good[:, 0], np.ones((num_points, 1))), axis=1)
+                all_v = np.concatenate((good[:, 1], np.ones((num_points, 1))), axis=1)
+                estimate_u = H @ all_v.T
+                estimate_u /= estimate_u[-1]
+                errors = np.linalg.norm(all_u - estimate_u.T, ord=1, axis=1)
+                num_inliers = sum(errors < 4)
+                if num_inliers > num_best_inliers:
+                    num_best_inliers = num_inliers
+                    best_H = H
 
         # TODO: 3. chain the homographies
+        last_best_H = last_best_H @ best_H
 
         # TODO: 4. apply warping
+        current_w += im1.shape[1]
+        out = warping(im2, dst, last_best_H, 0, im2.shape[0], current_w, current_w + im2.shape[1], direction='b') 
 
     return out
 
